1b8d31f75b63a1e71e4555842465215b2f0487ed
vietnv <nguyenvanvietbn1998@gmail.com>
2025-07-21 17:22:38 +0700

    ss

--- src/app/rustclash-connecting/page.js
+++ src/app/rustclash-connecting/page.js
@@ -16,7 +16,7 @@ export default function page() {
     <div>
       <main className={styles.main}>
         <h3 className={styles.text}>
-        RustClash Official Site{" "}
+        RustClash Official Site{""}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
